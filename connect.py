b63cce91bded514ba09e7efd5ef188766e625acb
Posudin Mykola <posudinmykola@gmail.com>
2024-11-27 14:10:46 +0100

    added 2D Array, which doesnt work

--- connect.py
+++ connect.py
@@ -14,12 +14,15 @@ from turtle import *
 from freegames import line
 
 turns = {'red': 'yellow', 'yellow': 'red'}
-state = {'player': 'yellow', 'rows': [0] * 8}
+state = {'player': 'red', 'rows': [0] * 8}
+# Инициализация 8x8 массива с None в каждой ячейке
+field = [[None for _ in range(8)] for _ in range(8)]
+#grid1 = [[None for _ in range(state['rows'])] for _ in range(state['rows'])]
 
 
 def grid():
     """Draw Connect Four grid."""
-    bgcolor('light blue')
+    bgcolor('blue')
 
     for x in range(-150, 200, 50):
         line(x, -200, x, 200)
@@ -28,19 +31,24 @@ def grid():
         for y in range(-175, 200, 50):
             up()
             goto(x, y)
-            dot(40, 'white')
+            dot(40, 'pink')
 
     update()
 
 
 def tap(x, y):
     """Draw red or yellow circle in tapped row."""
+    
     player = state['player']
+    print(player)
+    
     rows = state['rows']
-
+    
     row = int((x + 200) // 50)
+    print(row)
     count = rows[row]
-
+    print(count)
+    field[count][row] = player
     x = ((x + 200) // 50) * 50 - 200 + 25
     y = count * 50 - 200 + 25
 
@@ -48,9 +56,14 @@ def tap(x, y):
     goto(x, y)
     dot(40, player)
     update()
-
+    
     rows[row] = count + 1
     state['player'] = turns[player]
+    for rowOnField in field:
+        for elementOnField in rowOnField:
+            print(element, end='')
+        print()
+ 
 
 
 setup(420, 420, 370, 0)
